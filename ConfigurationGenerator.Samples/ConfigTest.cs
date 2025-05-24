dee4f0019c92b6004e4abbf0acf8172f71ccc5a3
fumiko <2394425147@qq.com>
2025-05-25 00:19:31 +0800

    feat: Initial commit

---
+++ ConfigurationGenerator.Samples/ConfigTest.cs
@@ -0,0 +1,13 @@
+﻿using BepInEx.Configuration.Generators;
+
+namespace ConfigurationGenerator.Samples;
+
+[GenerateConfig]
+public static partial class ConfigTest
+{
+    [Entry("Test", "Test1", 0, "Description")]
+    public static partial int Test { get; }
+
+    [Entry("Test", "Test 2", (short)1321, "Another test")]
+    public static partial short Test2 { get; }
+}
---
+++ ConfigurationGenerator.Samples/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.Mono;
+
+namespace ConfigurationGenerator.Samples;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public sealed class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+    private void Awake()
+    {
+        ConfigTest.Register(Config);
+
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
